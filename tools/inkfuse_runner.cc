37a04fca1e7a279ed1eee17b66d14c9aaeceec5a
Benjamin Wagner <benjamin.wagner@firebolt.io>
2022-02-20 22:59:35 +0100

    Set up runner

--- tools/inkfuse_runner.cc
+++ tools/inkfuse_runner.cc
@@ -1,21 +1,18 @@
 // InkFuse main binary to run queries on top of TPC-H
 #include "gflags/gflags.h"
+#include "exec/ExecutionContext.h"
 
 namespace {
 
 } // namespace
 
-DEFINE_string(include_dir, "", "Path to imlabdb include directories");
-
-static bool ValidateFlag(const char* flagname, const std::string& value) {
-   return !value.empty();
-}
-
-DEFINE_validator(include_dir, &ValidateFlag);
+using namespace inkfuse;
 
 int main(int argc, char* argv[]) {
-   gflags::SetUsageMessage("imlabdb --include_dir <path to imlab include dir>");
+   gflags::SetUsageMessage("inkfuse_runner");
    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+
 
    return 0;
 }
