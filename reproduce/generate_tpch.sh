4018234fbc91b1bad43671331cf5f643df198857
Benjamin Wagner <benjamin.wagner@firebolt.io>
2023-01-07 18:30:34 +0100

    Add HyperAPI Reproducibility Script

--- reproduce/generate_tpch.sh
+++ reproduce/generate_tpch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $# < 1 ]]; then
-  echo "Run as ./generate_tpch.sh <scale factor> <system={duckdb|inkfuse}>"
+  echo "Run as ./generate_tpch.sh <scale factor> <system={duckdb|hyper|inkfuse}>"
   exit 1
 fi
 
@@ -21,8 +21,13 @@ rm -rf tpch-dbgen
 
 # Normalize data for system ingest
 if [[ $# -ge 2 ]]; then
-  if [[ $2 -eq "duckdb" ]]; then
+  if [[ $2 == "duckdb" ]]; then
     echo "Cleaning up .tbl for DuckDB"
+    # Remove trailing '|' which make this invalid CSV
+    sed -i 's/.$//' data/*.tbl
+  fi
+  if [[ $2 == "hyper" ]]; then
+    echo "Cleaning up .tbl for Hyper"
     # Remove trailing '|' which make this invalid CSV
     sed -i 's/.$//' data/*.tbl
   fi
