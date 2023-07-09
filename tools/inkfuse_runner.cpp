616bba8bdc71225d14446ba45b5bda2191e44b6c
Benjamin Wagner <benjamin.wagner@firebolt.io>
2023-07-09 19:48:51 +0200

    Update Inkfuse Runner to Support multiple Threads

--- tools/inkfuse_runner.cpp
+++ tools/inkfuse_runner.cpp
@@ -7,8 +7,8 @@
 #include "storage/Relation.h"
 #include <chrono>
 #include <deque>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <vector>
 
 namespace {
@@ -22,6 +22,8 @@ load sf<X> - load TPC-H data in "/sf<X>"
 show - show what scale factor that got loaded
 mute - don't show query text
 unmute - do show query text
+threads <K> - set number of worker threads to k, default 1  
+mode <ExecMode> - change default execution mode  
 run q<N> [mode <ExecMode>] - run TPC-H query <N> on the loaded sf<X>
                              optional ExecMode in {Compiled, Interpreted, Hybrid}
                              default Hybrid.
@@ -39,7 +41,7 @@ std::vector<std::string> splitCommand(const std::string& command) {
    return elems;
 }
 
-void runQuery(const std::string& q_name, std::unique_ptr<Print> root, PipelineExecutor::ExecutionMode mode) {
+void runQuery(const std::string& q_name, std::unique_ptr<Print> root, PipelineExecutor::ExecutionMode mode, size_t num_threads) {
    static size_t q_id = 0;
    std::ifstream input("q/" + q_name + ".sql");
    if (!mute && input.is_open()) {
@@ -52,7 +54,7 @@ void runQuery(const std::string& q_name, std::unique_ptr<Print> root, PipelineEx
    printer->setOstream(std::cout);
    auto control_block = std::make_shared<PipelineExecutor::QueryControlBlock>(std::move(root));
    auto start = std::chrono::steady_clock::now();
-   QueryExecutor::runQuery(control_block, mode, q_name + "_" + std::to_string(q_id++));
+   QueryExecutor::runQuery(control_block, mode, q_name + "_" + std::to_string(q_id++), num_threads);
    auto stop = std::chrono::steady_clock::now();
    std::cout << "Produced " << printer->num_rows << " rows after ";
    std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() << "ms\n";
@@ -76,6 +78,20 @@ int main(int argc, char* argv[]) {
    // Only ingest data once and share it across tests.
    std::optional<Schema> loaded;
    std::string loaded_name;
+
+   size_t thread_count = 1;
+   PipelineExecutor::ExecutionMode default_mode = PipelineExecutor::ExecutionMode::Hybrid;
+   auto parse_mode = [](std::string str) -> std::optional<PipelineExecutor::ExecutionMode> {
+      if (str == "Compiled") {
+         return PipelineExecutor::ExecutionMode::Fused;
+      } else if (str == "Interpreted") {
+         return PipelineExecutor::ExecutionMode::Interpreted;
+      } else if (str == "Hybrid") {
+         return PipelineExecutor::ExecutionMode::Hybrid;
+      } else {
+         return std::nullopt;
+      }
+   };
 
    for (std::string command; std::getline(std::cin, command);) {
       std::vector<std::string> split = splitCommand(command);
@@ -112,8 +128,31 @@ int main(int argc, char* argv[]) {
                std::cout << "Loaded after " << std::chrono::duration_cast<std::chrono::seconds>(end - start).count() << " seconds" << std::endl;
                loaded_name = split[1];
             }
+         } else if (split[0] == "threads") {
+            if (split.size() < 2) {
+               std::cout << "invoke 'load' as 'load <K>'\n"
+                         << std::endl;
+            } else {
+               std::cout << "Setting threads to " << split[1] << std::endl;
+               thread_count = std::stoull(split[1]);
+            }
+         } else if (split[0] == "mode") {
+            if (split.size() < 2) {
+               std::cout << "invoke 'mode' as 'mode <ExecMode>' where <ExecMode> in {Compiled|Interpreted|Hybrid}\n"
+                         << std::endl;
+            } else {
+               std::optional<PipelineExecutor::ExecutionMode> res = parse_mode(split[1]);
+               if (res) {
+                  std::cout << "Setting default mode to " << split[1] << std::endl;
+                  default_mode = *res;
+               } else {
+                  std::cout << "Unrecognized execution mode - we only support {Compiled|Interpreted|Hybrid}\n"
+                            << std::endl;
+                  continue;
+               }
+            }
          } else if (split[0] == "run") {
-            auto mode = PipelineExecutor::ExecutionMode::Hybrid;
+            auto mode = default_mode;
             if (loaded_name.empty()) {
                std::cout << "You need to load data first through `load sf<X>`\n"
                          << std::endl;
@@ -122,48 +161,38 @@ int main(int argc, char* argv[]) {
                          << std::endl;
             } else {
                if (split.size() > 2 && split[2] == "mode") {
-                  if (split[3] == "Compiled") {
-                     mode = PipelineExecutor::ExecutionMode::Fused;
-                  } else if (split[3] == "Interpreted") {
-                     mode = PipelineExecutor::ExecutionMode::Interpreted;
-                  } else if (split[3] == "Hybrid") {
-                     mode = PipelineExecutor::ExecutionMode::Hybrid;
-                  } else {
-                     std::cout << "Unrecognized execution mode - we only support {Compiled|Interpreted|Hybrid}\n" << std::endl;
+                  std::optional<PipelineExecutor::ExecutionMode> res = parse_mode(split[3]);
+                  if (!res) {
+                     std::cout << "Unrecognized execution mode - we only support {Compiled|Interpreted|Hybrid}\n"
+                               << std::endl;
                      continue;
                   }
+                  mode = *res;
                }
                if (split[1] == "q1") {
                   auto q = tpch::q1(*loaded);
-                  runQuery("q1", std::move(q), mode);
-               }
-               else if (split[1] == "q3") {
+                  runQuery("q1", std::move(q), mode, thread_count);
+               } else if (split[1] == "q3") {
                   auto q = tpch::q3(*loaded);
-                  runQuery("q3", std::move(q), mode);
-               }
-               else if (split[1] == "q4") {
+                  runQuery("q3", std::move(q), mode, thread_count);
+               } else if (split[1] == "q4") {
                   auto q = tpch::q4(*loaded);
-                  runQuery("q4", std::move(q), mode);
-               }
-               else if (split[1] == "q6") {
+                  runQuery("q4", std::move(q), mode, thread_count);
+               } else if (split[1] == "q6") {
                   auto q = tpch::q6(*loaded);
-                  runQuery("q6", std::move(q), mode);
-               }
-               else if (split[1] == "q14") {
+                  runQuery("q6", std::move(q), mode, thread_count);
+               } else if (split[1] == "q14") {
                   auto q = tpch::q14(*loaded);
-                  runQuery("q14", std::move(q), mode);
-               }
-               else if (split[1] == "q18") {
+                  runQuery("q14", std::move(q), mode, thread_count);
+               } else if (split[1] == "q18") {
                   auto q = tpch::q18(*loaded);
-                  runQuery("q18", std::move(q), mode);
-               }
-               else if (split[1] == "l_count") {
+                  runQuery("q18", std::move(q), mode, thread_count);
+               } else if (split[1] == "l_count") {
                   auto q = tpch::l_count(*loaded);
-                  runQuery("l_count", std::move(q), mode);
-               }
-               else if (split[1] == "l_point") {
+                  runQuery("l_count", std::move(q), mode, thread_count);
+               } else if (split[1] == "l_point") {
                   auto q = tpch::l_point(*loaded);
-                  runQuery("l_point", std::move(q), mode);
+                  runQuery("l_point", std::move(q), mode, thread_count);
                } else {
                   std::cout << "Unrecognized query - we only support {q1, q3, q4, q6, q14, q18, l_count, l_point}\n";
                }
