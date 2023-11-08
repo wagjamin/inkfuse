b10983744b2b2f30badde54eedc5b3b63201a0c8
Benjamin Wagner <benjamin.wagner@firebolt.io>
2023-11-08 23:29:33 +0100

    Fix Umbra Reproduction

--- reproduce/sql/load.sql
+++ reproduce/sql/load.sql
@@ -1,8 +1,8 @@
 copy part from 'data/part.tbl' delimiter '|';
--- copy supplier from 'supplier.tbl' delimiter '|';
--- copy partsupp from 'partsupp.tbl' delimiter '|';
+copy supplier from 'data/supplier.tbl' delimiter '|';
+-- copy partsupp from 'data/partsupp.tbl' delimiter '|';
 copy customer from 'data/customer.tbl' delimiter '|';
 copy orders from 'data/orders.tbl' delimiter '|';
 copy lineitem from 'data/lineitem.tbl' delimiter '|';
--- copy nation from 'nation.tbl' delimiter '|';
--- copy region from 'region.tbl' delimiter '|';
+copy nation from 'data/nation.tbl' delimiter '|';
+copy region from 'data/region.tbl' delimiter '|';
